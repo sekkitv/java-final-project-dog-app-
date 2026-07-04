8225d5584bf53a9a91689d4c74324833a4daf3fa
Liran <Lirana437@gmail.com>
2026-07-04 17:42:47 +0300

    Initial commit: React + Vite frontend setup

---
+++ frontend/src/context/AppContext.jsx
@@ -0,0 +1,14 @@
+/* eslint-disable react-refresh/only-export-components */
+import { createContext, useState } from 'react';
+
+export const AppContext = createContext();
+
+export const AppProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+
+  return (
+    <AppContext.Provider value={{ user, setUser }}>
+      {children}
+    </AppContext.Provider>
+  );
+};
